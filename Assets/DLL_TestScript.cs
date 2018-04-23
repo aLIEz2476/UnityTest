6285605a0d2ac94fb640301ef02031bec7e79b49
aLIEz2476 <sakuragun11@gmail.com>
2018-04-23 15:42:54 +0900

    1
    1

---
+++ Assets/DLL_TestScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TestLibrary;
+
+public class DLL_TestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log(TestLibrary.Class1.DLLTest(10));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
