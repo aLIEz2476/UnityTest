f115a4ed65d7ded26fb6bbc665c3933234b03239
aLIEz2476 <sakuragun11@gmail.com>
2018-10-15 00:55:49 +0900

    DLL_Test
    DLL_Test

---
+++ Assets/PluginManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PluginManager : MonoBehaviour {
+    public int n_Time;
+    public string str_Msg;
+#if UNITY_ANDROID
+    private AndroidJavaObject m_AndroidJavaObject=null;
+    private AndroidJavaObject m_ActivityInstance=null;
+#elif UNITY_IOS
+#endif
+    int nNativeData = 0;
+    // Use this for initialization
+    void Awake () {
+#if UNITY_ANDROID
+        using (AndroidJavaClass unityPlayerClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+        {
+            m_ActivityInstance = unityPlayerClass.GetStatic<AndroidJavaObject>("currentActivity");
+        }
+
+        m_AndroidJavaObject = new AndroidJavaObject("com.sbsgame.android.unityandroidplugin.Plugin");
+        Debug.LogWarning("PluginManager.AndroidJavaObject : " + m_AndroidJavaObject);
+#endif
+    }
+    private void Start()
+    {
+        
+    }
+    int GetInt()
+    {
+        int nResult = -1;
+#if UNITY_ANDROID
+        nResult = m_AndroidJavaObject.Call<int>("GetInt");
+#endif
+        return nResult;
+    }
+
+    void ToastTest()
+    {
+        m_AndroidJavaObject.Call("SetActivity", m_ActivityInstance);
+        m_AndroidJavaObject.Call("ToastTest", str_Msg, n_Time);
+        
+    }
+
+    private void OnGUI()
+    {
+        
+        if(GUI.Button(new Rect(300, 300, 1000, 200), "TestButton"))
+        {
+            ToastTest();
+        }
+       
+        nNativeData = GetInt();
+        if (m_AndroidJavaObject != null)
+        {
+            GUI.Box(new Rect(0, 0, 200, 20), "GetInt : " + nNativeData);
+        }
+        else
+            GUI.Box(new Rect(0, 0, 200, 20), "Plugin Load Failed!");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/SqlSender.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SqlSender : MonoBehaviour {
+
+    public string m_strServerAddr = "127.0.0.1";
+    public string m_strDataBase = "test";
+
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(0, 0, 100, 100), "Send"))
+        {
+            string strQuery = "SELECT * FROM ncsrpg";
+            eResultMode mode = eResultMode.SELECT;
+            StartCoroutine(GetSqlResult(mode, strQuery));
+        }
+    }
+
+    public enum eResultMode { CHECK, SELECT};
+
+    public string GetSelectQuery(string table, string where)
+    {
+        return string.Format("SELECT * FROM {0} WHERE {1}", table, where);
+    }
+
+    public string GetInsertQuery(string table, string ids, string values)
+    {
+        return string.Format("INSERT INTO {0} ({1}) VALUES ({2})", table, ids, values);
+    }
+
+    IEnumerator GetSqlResult(eResultMode mode, string sql)
+    {
+        WWWForm form = new WWWForm();
+
+        form.AddField("servername", m_strServerAddr);
+        form.AddField("database", m_strDataBase);
+        form.AddField("mode", string.Format("{0}", (int)mode));
+        form.AddField("sql", sql);
+        Debug.Log(string.Format("servername:{0} database:{1} sql:{2}", m_strServerAddr, m_strDataBase, sql));
+
+        WWW webRequest = new WWW(string.Format("http://{0}/sql_query_result.php"
+            , m_strServerAddr), form);
+        yield return webRequest;
+
+        if(webRequest.isDone)
+        {
+            Debug.Log("Find:"+webRequest.text);
+        }
+        else
+        {
+            Debug.LogError("Filed : " + webRequest.error);
+        }
+        
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
